54aaebb3faad369a8e4a95a624823b53efe41f64
DforceJ <cdj7754@gmail.com>
2026-04-07 20:56:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ try:
     # ------------------ 좌측: 데이터 필터 영역 ------------------
     with col_filter:
         # 1. 'st.header'를 'st.markdown'으로 바꾸고 앞에 '###' 추가
-        st.markdown("### 🔍 데이터 필터") 
+        st.markdown("##### 🔍 데이터 필터") 
         
         selected_devices = []
         with st.expander("✅ 측정 위치", expanded=True):
@@ -90,7 +90,7 @@ try:
         st.markdown("---")
         
         # 2. 'st.subheader'를 'st.markdown'으로 바꾸고 앞에 '####' 추가
-        st.markdown("#### 📅 측정 일자 및 시간")
+        st.markdown("##### 📅 측정 일자 및 시간")
         
         def render_filter(label, item_list, key_p):
             sel = []
